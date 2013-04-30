3e62d0d0128e334c0152e5944075721d2379d041
Brendon Jones <brendonj@waikato.ac.nz>
2013-04-30 02:21:27 +0000

    Convert xferd to a simple python program for reading from a rabbitmq queue.

---
+++ src/xferd/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name="xferd",
+    version="1.0",
+    description="AMP xferd daemon package",
+    author='Brendon Jones',
+    author_email='contact@wand.net.nz',
+    url='http://www.wand.net.nz',
+    )
