04e7b466e5dc120c0932086d11b99d4e488352ed
Brendon Jones <brendonj@waikato.ac.nz>
2013-11-15 03:09:07 +0000

    All files/dirs are now named "amplet" rather than "measured" or "amplet2".
    The package name should probably change to be something along the lines of "amplet" and "amplet-lite", but that can be done later. This is a good first step towards getting everything consistent, and not clashing with files from the previous versions of AMP.

--- src/measured/measured.c
+++ src/measured/measured.c
@@ -293,10 +293,10 @@ int main(int argc, char *argv[]) {
 	};
     }
 
-    Log(LOG_INFO, "measured starting");
+    Log(LOG_INFO, "amplet starting");
 
     if ( !config_file ) {
-	config_file = AMP_CONFIG_DIR "/measured.conf";
+	config_file = AMP_CONFIG_DIR "/amplet.conf";
     }
 
     if ( parse_config(config_file, &vars) < 0 ) {
