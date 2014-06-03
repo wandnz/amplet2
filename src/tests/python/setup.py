d5568b05ed7fee164f3569e0858ae48f37686c4a
Shane Alcock <salcock@waikato.ac.nz>
2014-06-04 09:50:07 +1200

    Use setuptools by default rather than deprecated distutils

--- src/tests/python/setup.py
+++ src/tests/python/setup.py
@@ -1,9 +1,12 @@
-from distutils.core import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
 setup(name="ampsave",
     version="1.0",
     description="AMP xferd data storage package",
-    author='Brendon Jones',
+    author="Brendon 'Fuckface' Jones",
     author_email='contact@wand.net.nz',
     url='http://www.wand.net.nz',
     packages=['ampsave', 'ampsave.tests'],
