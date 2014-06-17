b3658a4a4a156928ce10f4695493b889ffd744c5
Brendon Jones <brendonj@waikato.ac.nz>
2014-06-17 16:34:27 +1200

    Removed unused duplicate initialisation function.

--- src/measured/nssock.h
+++ src/measured/nssock.h
@@ -7,8 +7,6 @@
 #define MAX_RESOLVER_SOCKET_BACKLOG 16
 
 int initialise_resolver_socket(char *path);
-//struct ub_ctx *amp_resolver_context_init(char *servers[], int nscount,
-//        char *sourcev4, char *sourcev6);
 void resolver_socket_event_callback(struct wand_fdcb_t *handle,
         __attribute__((unused))enum wand_eventtype_t ev);
 
