76c2349c7da398e14d618291c3044e907667e713
Brendon Jones <brendonj@waikato.ac.nz>
2014-08-07 15:50:48 +1200

    Update schedule unit tests to use new functions.

--- src/measured/test/schedule_time_test.c
+++ src/measured/test/schedule_time_test.c
@@ -19,15 +19,16 @@ typedef struct test_schedule {
  */
 static void check_period_time(void) {
     /* check the Hourly, Daily, Weekly identifiers get the correct duration */
-    assert(amp_test_get_period_max_value('H') == 60 * 60);
-    assert(amp_test_get_period_max_value('D') == 60 * 60 * 24);
-    assert(amp_test_get_period_max_value('W') == 60 * 60 * 24 * 7);
+    assert(amp_test_get_period_max_value(SCHEDULE_PERIOD_HOURLY) == 60*60);
+    assert(amp_test_get_period_max_value(SCHEDULE_PERIOD_DAILY) == 60*60*24);
+    assert(amp_test_get_period_max_value(SCHEDULE_PERIOD_WEEKLY) == 60*60*24*7);
 
     /* check that the period start values are the start of the period */
-    assert(amp_test_get_period_start('H') % 60 == 0);
-    assert(amp_test_get_period_start('D') % 86400 == 0);
+    assert(amp_test_get_period_start(SCHEDULE_PERIOD_HOURLY) % 60 == 0);
+    assert(amp_test_get_period_start(SCHEDULE_PERIOD_DAILY) % 86400 == 0);
     /* weekly period is awkward cause 01-01-1970 is Thursday and Sunday is 0 */
-    assert((amp_test_get_period_start('W') + 60*60*24*4) % 604800 == 0);
+    assert((amp_test_get_period_start(SCHEDULE_PERIOD_WEEKLY) +
+                60*60*24*4) % 604800 == 0);
 }
 
 
@@ -37,43 +38,52 @@ static void check_period_time(void) {
  */
 static void check_time_parsing(void) {
     /* check that hourly repeated times are correctly validated */
-    assert(amp_test_get_time_value("0", 'H') == 0);
-    assert(amp_test_get_time_value("1000", 'H') == 1000);
-    assert(amp_test_get_time_value("60000", 'H') == 60000);
-    assert(amp_test_get_time_value("3600000", 'H') == 3600000);
-    assert(amp_test_get_time_value("-1", 'H') == -1);
-    assert(amp_test_get_time_value("-12345", 'H') == -1);
-    assert(amp_test_get_time_value("3600001", 'H') == -1);
-    assert(amp_test_get_time_value("86400000", 'H') == -1);
-    assert(amp_test_get_time_value("86400001", 'H') == -1);
-    assert(amp_test_get_time_value("604800000", 'H') == -1);
-    assert(amp_test_get_time_value("604800001", 'H') == -1);
+    assert(amp_test_check_time_range(0, SCHEDULE_PERIOD_HOURLY) == 0);
+    assert(amp_test_check_time_range(1000, SCHEDULE_PERIOD_HOURLY) == 1000);
+    assert(amp_test_check_time_range(60000, SCHEDULE_PERIOD_HOURLY) == 60000);
+    assert(amp_test_check_time_range(3600000,
+                SCHEDULE_PERIOD_HOURLY) == 3600000);
+    assert(amp_test_check_time_range(-1, SCHEDULE_PERIOD_HOURLY) == -1);
+    assert(amp_test_check_time_range(-12345, SCHEDULE_PERIOD_HOURLY) == -1);
+    assert(amp_test_check_time_range(3600001, SCHEDULE_PERIOD_HOURLY) == -1);
+    assert(amp_test_check_time_range(86400000, SCHEDULE_PERIOD_HOURLY) == -1);
+    assert(amp_test_check_time_range(86400001, SCHEDULE_PERIOD_HOURLY) == -1);
+    assert(amp_test_check_time_range(604800000, SCHEDULE_PERIOD_HOURLY) == -1);
+    assert(amp_test_check_time_range(604800001, SCHEDULE_PERIOD_HOURLY) == -1);
 
     /* check that daily repeated times are correctly validated */
-    assert(amp_test_get_time_value("0", 'D') == 0);
-    assert(amp_test_get_time_value("1000", 'D') == 1000);
-    assert(amp_test_get_time_value("60000", 'D') == 60000);
-    assert(amp_test_get_time_value("3600000", 'D') == 3600000);
-    assert(amp_test_get_time_value("3600001", 'D') == 3600001);
-    assert(amp_test_get_time_value("86400000", 'D') == 86400000);
-    assert(amp_test_get_time_value("-1", 'D') == -1);
-    assert(amp_test_get_time_value("-12345", 'D') == -1);
-    assert(amp_test_get_time_value("86400001", 'D') == -1);
-    assert(amp_test_get_time_value("604800000", 'D') == -1);
-    assert(amp_test_get_time_value("604800001", 'D') == -1);
+    assert(amp_test_check_time_range(0, SCHEDULE_PERIOD_DAILY) == 0);
+    assert(amp_test_check_time_range(1000, SCHEDULE_PERIOD_DAILY) == 1000);
+    assert(amp_test_check_time_range(60000, SCHEDULE_PERIOD_DAILY) == 60000);
+    assert(amp_test_check_time_range(3600000,
+                SCHEDULE_PERIOD_DAILY) == 3600000);
+    assert(amp_test_check_time_range(3600001,
+                SCHEDULE_PERIOD_DAILY) == 3600001);
+    assert(amp_test_check_time_range(86400000,
+                SCHEDULE_PERIOD_DAILY) == 86400000);
+    assert(amp_test_check_time_range(-1, SCHEDULE_PERIOD_DAILY) == -1);
+    assert(amp_test_check_time_range(-12345, SCHEDULE_PERIOD_DAILY) == -1);
+    assert(amp_test_check_time_range(86400001, SCHEDULE_PERIOD_DAILY) == -1);
+    assert(amp_test_check_time_range(604800000, SCHEDULE_PERIOD_DAILY) == -1);
+    assert(amp_test_check_time_range(604800001, SCHEDULE_PERIOD_DAILY) == -1);
 
     /* check that weekly repeated times are correctly validated */
-    assert(amp_test_get_time_value("0", 'W') == 0);
-    assert(amp_test_get_time_value("1000", 'W') == 1000);
-    assert(amp_test_get_time_value("60000", 'W') == 60000);
-    assert(amp_test_get_time_value("3600000", 'W') == 3600000);
-    assert(amp_test_get_time_value("3600001", 'W') == 3600001);
-    assert(amp_test_get_time_value("86400000", 'W') == 86400000);
-    assert(amp_test_get_time_value("86400001", 'W') == 86400001);
-    assert(amp_test_get_time_value("604800000", 'W') == 604800000);
-    assert(amp_test_get_time_value("-1", 'W') == -1);
-    assert(amp_test_get_time_value("-12345", 'W') == -1);
-    assert(amp_test_get_time_value("604800001", 'W') == -1);
+    assert(amp_test_check_time_range(0, SCHEDULE_PERIOD_WEEKLY) == 0);
+    assert(amp_test_check_time_range(1000, SCHEDULE_PERIOD_WEEKLY) == 1000);
+    assert(amp_test_check_time_range(60000, SCHEDULE_PERIOD_WEEKLY) == 60000);
+    assert(amp_test_check_time_range(3600000,
+                SCHEDULE_PERIOD_WEEKLY) == 3600000);
+    assert(amp_test_check_time_range(3600001,
+                SCHEDULE_PERIOD_WEEKLY) == 3600001);
+    assert(amp_test_check_time_range(86400000,
+                SCHEDULE_PERIOD_WEEKLY) == 86400000);
+    assert(amp_test_check_time_range(86400001,
+                SCHEDULE_PERIOD_WEEKLY) == 86400001);
+    assert(amp_test_check_time_range(604800000,
+                SCHEDULE_PERIOD_WEEKLY) == 604800000);
+    assert(amp_test_check_time_range(-1, SCHEDULE_PERIOD_WEEKLY) == -1);
+    assert(amp_test_check_time_range(-12345, SCHEDULE_PERIOD_WEEKLY) == -1);
+    assert(amp_test_check_time_range(604800001, SCHEDULE_PERIOD_WEEKLY) == -1);
 
 }
 
@@ -88,33 +98,60 @@ static void check_next_schedule_time(void) {
     int i, count;
     struct test_schedule schedule[] = {
         /* daily */
-        {'D', {0, 0}, 0, 86400000, 30000},          /* period start */
-        {'D', {30, 0}, 0, 86400000, 30000},         /* period end */
-        {'D', {1, 0}, 0, 86400000, 60000},          /* 1s into period */
-        {'D', {89, 0}, 0, 86400000, 90000},         /* 1s before period end */
-        {'D', {0, 500000}, 0, 86400000, 30000},     /* 0.5s into period */
-        {'D', {10, 900000}, 0, 86400000, 60000},    /* 10.9s into period */
-        {'D', {9, 900000}, 0, 10000, 10000},        /* very early end time */
-        {'D', {10, 900000}, 0, 10000, 10000},       /* very early end time */
-        {'D', {86398, 123000}, 0, 80000000, 10000}, /* early end time, cycle */
-        {'D', {86398, 123000}, 0, 86400000, 10000}, /* cycle to next period */
-        {'D', {123, 123000}, 0, 0, 0},              /* frequency 0 */
-        {'D', {35, 987000}, 60000, 0, 0},           /* freq 0, late start */
-        {'D', {61, 0}, 360000, 86400000, 60000},    /* late start before time */
-        {'D', {361, 0}, 360000, 86400000, 60000},   /* late start after time */
+        /* period start */
+        {SCHEDULE_PERIOD_DAILY, {0, 0}, 0, 86400000, 30000},
+        /* period end */
+        {SCHEDULE_PERIOD_DAILY, {30, 0}, 0, 86400000, 30000},
+        /* 1s into period */
+        {SCHEDULE_PERIOD_DAILY, {1, 0}, 0, 86400000, 60000},
+        /* 1s before period end */
+        {SCHEDULE_PERIOD_DAILY, {89, 0}, 0, 86400000, 90000},
+        /* 0.5s into period */
+        {SCHEDULE_PERIOD_DAILY, {0, 500000}, 0, 86400000, 30000},
+        /* 10.9s into period */
+        {SCHEDULE_PERIOD_DAILY, {10, 900000}, 0, 86400000, 60000},
+        /* very early end time */
+        {SCHEDULE_PERIOD_DAILY, {9, 900000}, 0, 10000, 10000},
+        /* very early end time */
+        {SCHEDULE_PERIOD_DAILY, {10, 900000}, 0, 10000, 10000},
+        /* early end time, cycle */
+        {SCHEDULE_PERIOD_DAILY, {86398, 123000}, 0, 80000000, 10000},
+        /* cycle to next period */
+        {SCHEDULE_PERIOD_DAILY, {86398, 123000}, 0, 86400000, 10000},
+        /* frequency 0 */
+        {SCHEDULE_PERIOD_DAILY, {123, 123000}, 0, 0, 0},
+        /* freq 0, late start */
+        {SCHEDULE_PERIOD_DAILY, {35, 987000}, 60000, 0, 0},
+        /* late start before time */
+        {SCHEDULE_PERIOD_DAILY, {61, 0}, 360000, 86400000, 60000},
+        /* late start after time */
+        {SCHEDULE_PERIOD_DAILY, {361, 0}, 360000, 86400000, 60000},
+
         /* weekly */
-        {'W', {0, 0}, 0, 604800000, 30000},         /* period start */
-        {'W', {30, 0}, 0, 604800000, 30000},        /* period end */
-        {'W', {1, 0}, 0, 604800000, 60000},         /* 1s into period */
-        {'W', {604799, 0}, 0, 604800000, 90000},    /* 1s before period end */
-        {'W', {0, 567000}, 0, 604800000, 30000},    /* 0.567890 into period */
-        {'W', {10, 900000}, 0, 604800000, 60000},   /* 10.9s into period */
-        {'W', {68, 500000}, 0, 604800000, 86400000},/* 1 day frequency */
-        {'W', {10000, 0}, 0, 10000000, 10000},      /* last cycle in period */
-        {'W', {123, 123000}, 0, 0, 0},              /* frequency 0 */
-        {'W', {86000, 987000}, 86400000, 0, 0},     /* freq 0, late start */
-        {'W', {61, 0}, 360000, 604800000, 60000},   /* late start before time */
-        {'W', {361, 0}, 360000, 86400000, 60000},   /* late start after time*/
+        /* period start */
+        {SCHEDULE_PERIOD_WEEKLY, {0, 0}, 0, 604800000, 30000},
+        /* period end */
+        {SCHEDULE_PERIOD_WEEKLY, {30, 0}, 0, 604800000, 30000},
+        /* 1s into period */
+        {SCHEDULE_PERIOD_WEEKLY, {1, 0}, 0, 604800000, 60000},
+        /* 1s before period end */
+        {SCHEDULE_PERIOD_WEEKLY, {604799, 0}, 0, 604800000, 90000},
+        /* 0.567890 into period */
+        {SCHEDULE_PERIOD_WEEKLY, {0, 567000}, 0, 604800000, 30000},
+        /* 10.9s into period */
+        {SCHEDULE_PERIOD_WEEKLY, {10, 900000}, 0, 604800000, 60000},
+        /* 1 day frequency */
+        {SCHEDULE_PERIOD_WEEKLY, {68, 500000}, 0, 604800000, 86400000},
+        /* last cycle in period */
+        {SCHEDULE_PERIOD_WEEKLY, {10000, 0}, 0, 10000000, 10000},
+        /* frequency 0 */
+        {SCHEDULE_PERIOD_WEEKLY, {123, 123000}, 0, 0, 0},
+        /* freq 0, late start */
+        {SCHEDULE_PERIOD_WEEKLY, {86000, 987000}, 86400000, 0, 0},
+        /* late start before time */
+        {SCHEDULE_PERIOD_WEEKLY, {61, 0}, 360000, 604800000, 60000},
+        /* late start after time*/
+        {SCHEDULE_PERIOD_WEEKLY, {361, 0}, 360000, 86400000, 60000},
     };
     struct timeval expected[] = {
         /* daily */
