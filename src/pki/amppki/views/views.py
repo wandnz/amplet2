1b1c0ba43599f9991ef736ca0688a1970f78b487
Brendon Jones <brendonj@waikato.ac.nz>
2014-11-24 19:20:31 +1300

    (Poor) interim fix for the encoding of the CSR.

--- src/pki/amppki/views/views.py
+++ src/pki/amppki/views/views.py
@@ -6,6 +6,8 @@ from Crypto.Util.asn1 import DerSequence
 from Crypto.Hash import SHA256
 from Crypto.Signature import PKCS1_v1_5
 from ssl import PEM_cert_to_DER_cert
+from base64 import urlsafe_b64decode
+import urllib
 
 @view_config(route_name="default", renderer="string")
 def default(request):
@@ -21,23 +23,41 @@ def cacert(request):
     return open("cacert.pem").read()
 
 
+# XXX merge this with /cert and do different things on POST vs GET?
 @view_config(route_name="sign", renderer="string")
 def sign(request):
     # TODO can we make sure this is done over SSL? don't accept this otherwise
     print "signing a cert"
     print request.POST
     print request.body
+
+    if len(request.POST.keys()) != 1:
+        print "no csr in message"
+        #return HTTPBadRequest()
+        return Response(status_code=400)
+
     # first check if we have already signed this one, and send it if so (maybe
     # the client went away before it was signed).
-    csr = request.POST.keys()[0]
+    #csr = request.POST.keys()[0]
+    csr = urllib.unquote_plus(request.body)
+
+    # TODO verify this is actually a CSR
+    # TODO use a regex, or stop it being modified in the first place!
+    csr = csr.replace(" ", "+")
+    csr = csr.replace("BEGIN+CERTIFICATE+REQUEST", "BEGIN CERTIFICATE REQUEST")
+    csr = csr.replace("END+CERTIFICATE+REQUEST", "END CERTIFICATE REQUEST")
+    print csr
+
 
     # if there isn't one we've prepared earlier, check if we can auto-sign
     # this one right now (maybe it matches a known host config).
 
     # otherwise we add it to the queue and wait for a human to check it and
     # decide if it should be signed or not
+    print "saving csr"
     open("test.csr", "w").write(csr)
-    return HTTPAccepted()
+    #return HTTPAccepted()
+    return Response(status_code=202)
 
 
 # TODO PKCS1_PSS vs PKCS1_v1_5
