99f114630ffe7f735d423ea33fd8e6899c821e92
Brendon Jones <brendonj@waikato.ac.nz>
2014-12-09 16:52:33 +1300

    Disable TLS compression to mitigate CRIME attack.

--- src/common/ssl.c
+++ src/common/ssl.c
@@ -292,6 +292,9 @@ SSL_CTX* initialise_ssl_context(void) {
     ssl_ctx = SSL_CTX_new(SSLv23_method());
     SSL_CTX_set_options(ssl_ctx, SSL_OP_MIN_TLSv1_2);
 
+    /* disable compression to mitigate CRIME attack */
+    SSL_CTX_set_options(ssl_ctx, SSL_OP_NO_COMPRESSION);
+
     /* Make sure all clients provide a certificate, and that it is valid */
     SSL_CTX_set_verify(ssl_ctx,
             SSL_VERIFY_PEER | SSL_VERIFY_FAIL_IF_NO_PEER_CERT, NULL);
