c5461992c2ca33cda9bdf2753522f36db9fc34f4
Brendon Jones <brendonj@waikato.ac.nz>
2015-03-04 15:56:22 +1300

    Remove PKI files that are in their own repository.

--- src/pki/amppki/common.py
+++
@@ -1,7 +0,0 @@
-from re import search
-
-
-def verify_common_name(name):
-    if search("[^a-zA-Z0-9.-]+", name) is None:
-        return True
-    return False
--- src/pki/amppki/config.py
+++
@@ -1,4 +0,0 @@
-CA_DIR = "/tmp/brendonj/ampca2"
-CERT_DIR = "%s/certs" % CA_DIR
-CSR_DIR = "%s/csr" % CA_DIR
-RABBITMQCTL = "/usr/sbin/rabbitmqctl"
