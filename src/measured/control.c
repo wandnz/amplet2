b4a5fcf388534e6b3b5f94543360f5cdb22a5fae
Brendon Jones <brendonj@waikato.ac.nz>
2016-02-22 11:43:57 +1300

    Switch on control message type to allow different message types.

--- src/measured/control.c
+++ src/measured/control.c
@@ -65,26 +65,43 @@ static int parse_server_start(void *data, uint32_t len, test_type_t *type) {
 /*
  *
  */
-static int read_server_start(SSL *ssl, test_type_t *type) {
-      void *data;
-      int len;
+static void do_start_server(SSL *ssl, void *data, uint32_t len) {
+    test_type_t test_type;
+    test_t *test;
 
-      /* read the packet from the stream */
-      if ( (len=read_control_packet_ssl(ssl, &data)) < 0 ) {
-          Log(LOG_WARNING, "Failed to read SERVER packet");
-          return -1;
-      }
+    /* TODO read test arguments if required */
+    if ( parse_server_start(data, len, &test_type) < 0 ) {
+        Log(LOG_WARNING, "Failed to parse HELLO packet");
+        return;
+    }
 
-      /* validate it as a SERVER packet and then try to extract options */
-      if ( parse_server_start(data, len, type) < 0 ) {
-          Log(LOG_WARNING, "Failed to parse HELLO packet");
-          free(data);
-          return -1;
-      }
+    Log(LOG_DEBUG, "Read test id %d from control connection", test_type);
 
-      free(data);
+    /* Make sure it is a valid test id we are being asked to start */
+    if ( test_type >= AMP_TEST_LAST || test_type <= AMP_TEST_INVALID ) {
+        Log(LOG_DEBUG, "Read invalid test id on control socket: %d", test_type);
+        return;
+    }
 
-      return 0;
+    /* TODO limit number of connections/servers running, weighted system */
+
+    /* Make sure that the test has been built and loaded */
+    if ( (test = amp_tests[test_type]) == NULL ) {
+        Log(LOG_DEBUG, "No test module for test id: %d", test_type);
+        return;
+    }
+
+    /* Make sure that the test requires a server to be run */
+    if ( test->server_callback == NULL ) {
+        Log(LOG_DEBUG, "No server callback for %s test", test->name);
+        return;
+    }
+
+    /*
+     * Run server function using callback in test, give it the ssl descriptor
+     * so that it can report the port number.
+     */
+    test->server_callback(0, NULL, ssl);
 }
 
 
@@ -95,36 +112,10 @@ static int read_server_start(SSL *ssl, test_type_t *type) {
 static void process_control_message(int fd) {
     SSL *ssl;
     X509 *client_cert;
-    //struct sockaddr_storage peer;
-    //socklen_t addrlen = sizeof(struct sockaddr_storage);
-    //char hostname[NI_MAXHOST];
-    test_type_t test_id;
-    test_t *test;
-
+    int bytes;
+    void *data;
 
     Log(LOG_DEBUG, "Processing control message");
-
-    /*
-     * Get the name of the remote end before we even try to do anything SSL
-     * related - if we can't get the hostname we won't be able to validate
-     * it anyway.
-     */
-#if 0
-    if ( getpeername(fd, (struct sockaddr *)&peer, &addrlen) < 0 ) {
-        Log(LOG_WARNING, "Failed to get peer");
-        close(fd);
-        exit(0);
-    }
-
-    if ( getnameinfo((struct sockaddr *)&peer, addrlen, hostname, NI_MAXHOST,
-                NULL, 0, NI_NAMEREQD) < 0 ) {
-        Log(LOG_WARNING, "Failed to resolve peer hostname");
-        close(fd);
-        exit(0);
-    }
-
-    Log(LOG_DEBUG, "Remote host is named %s", hostname);
-#endif
 
     /* Open up the ssl channel and validate the cert against our CA cert */
     /* TODO CRL or OCSP to deal with revocation of certificates */
@@ -142,68 +133,29 @@ static void process_control_message(int fd) {
         exit(0);
     }
 
-    /*
-     * XXX turn this off for now, as relying on reverse DNS doesn't actually
-     * add a lot of useful security, and we don't have control over how it
-     * is set for most of our monitors. For now, we will accept a control
-     * connection from anyone that has a valid cert, as there doesn't appear
-     * to be anything too bad they can do (i.e. use us to attack others).
-     */
-#if 0
-    /* Validate that the client hostname matches the common name in the cert */
-    if ( matches_common_name(hostname, client_cert) != 0 ) {
-        Log(LOG_DEBUG, "Closing control connection to unverified client");
-        X509_free(client_cert);
-        ssl_shutdown(ssl);
-        close(fd);
-        Log(LOG_DEBUG, "Terminating control process, pid: %d", getpid());
-        exit(0);
-    }
-#endif
-
     X509_free(client_cert);
 
     Log(LOG_DEBUG, "Successfully validated peer cert");
 
-    /* TODO read test arguments if required */
+    while ( (bytes=read_control_packet_ssl(ssl, &data)) > 0 ) {
+        Amplet2__Measured__Control *msg;
+        msg = amplet2__measured__control__unpack(NULL, bytes, data);
 
-    if ( read_server_start(ssl, &test_id) < 0 ) {
-        /* Failed to read the test id, close connection and remove event */
-        ssl_shutdown(ssl);
-        close(fd);
-        return;
+        switch ( msg->type ) {
+            case AMPLET2__MEASURED__CONTROL__TYPE__SERVER: {
+                do_start_server(ssl, data, bytes);
+                break;
+            }
+
+            default: Log(LOG_WARNING, "Unhandled measured control message %d",
+                             msg->type);
+                     break;
+        };
+
+        /* both read_control_packet and unpacking the buffer allocate memory */
+        amplet2__measured__control__free_unpacked(msg, NULL);
+        free(data);
     }
-
-    Log(LOG_DEBUG, "Read test id %d from control connection", test_id);
-
-    /* Make sure it is a valid test id we are being asked to start */
-    if ( test_id >= AMP_TEST_LAST || test_id <= AMP_TEST_INVALID ) {
-        Log(LOG_DEBUG, "Read invalid test id on control socket: %d", test_id);
-        close(fd);
-        return;
-    }
-
-    /* TODO limit number of connections/servers running, weighted system */
-
-    /* Make sure that the test has been built and loaded */
-    if ( (test = amp_tests[test_id]) == NULL ) {
-        Log(LOG_DEBUG, "No test module for test id: %d", test_id);
-        close(fd);
-        return;
-    }
-
-    /* Make sure that the test requires a server to be run */
-    if ( test->server_callback == NULL ) {
-        Log(LOG_DEBUG, "No server callback for %s test", test->name);
-        close(fd);
-        return;
-    }
-
-    /*
-     * Run server function using callback in test, give it the ssl descriptor
-     * so that it can report the port number.
-     */
-    test->server_callback(0, NULL, ssl);
 
     ssl_shutdown(ssl);
     close(fd);
@@ -213,11 +165,7 @@ static void process_control_message(int fd) {
 
 
 /*
- * XXX UPDATE COMMENT
- * Read initial data from a control connection and stop triggering on this
- * socket. The first byte will be a test id that we need to validate and will
- * be used to set up watchdogs. After forking, that process will take care of
- * validating the SSL certs and actually running the server.
+ * Short callback to fork a new process for dealing with the control message.
  */
 static void control_read_callback(wand_event_handler_t *ev_hdl, int fd,
         __attribute__((unused))void *data,
@@ -230,43 +178,6 @@ static void control_read_callback(wand_event_handler_t *ev_hdl, int fd,
      * we read data from here it is someone elses problem.
      */
     wand_del_fd(ev_hdl, fd);
-
-#if 0
-    /* Read the first byte to get the test id. We will trust this value for
-     * now, before we verify the SSL certificate - there isn't much harm that
-     * can come from it, and if it is bogus we bail out very quickly.
-     */
-    if ( recv(fd, &test_id, sizeof(uint8_t), 0) < 0 ) {
-        /* Failed to read the test id, close connection and remove event */
-        close(fd);
-        return;
-    }
-
-    Log(LOG_DEBUG, "Read test id %d from control connection", test_id);
-
-    /* Make sure it is a valid test id we are being asked to start */
-    if ( test_id >= AMP_TEST_LAST || test_id <= AMP_TEST_INVALID ) {
-        Log(LOG_DEBUG, "Read invalid test id on control socket: %d", test_id);
-        close(fd);
-        return;
-    }
-
-    /* TODO limit number of connections/servers running, weighted system */
-
-    /* Make sure that the test has been built and loaded */
-    if ( (test = amp_tests[test_id]) == NULL ) {
-        Log(LOG_DEBUG, "No test module for test id: %d", test_id);
-        close(fd);
-        return;
-    }
-
-    /* Make sure that the test requires a server to be run */
-    if ( test->server_callback == NULL ) {
-        Log(LOG_DEBUG, "No server callback for %s test", test->name);
-        close(fd);
-        return;
-    }
-#endif
 
     /* Fork to validate SSL cert and actually run the server */
     if ( (pid = fork()) < 0 ) {
@@ -282,12 +193,20 @@ static void control_read_callback(wand_event_handler_t *ev_hdl, int fd,
     /* the parent process doesn't need the client file descriptor */
     close(fd);
 
+    /*
+     * TODO can't start a watchdog now, as the control message could be doing
+     * something other than starting a test server. The test server process
+     * can't really add this, can anyone, do we care anymore?
+     */
+    /*
+     * TODO when this child terminates, it tries to find a watchdog timer with
+     * this pid to kill, but it now doesn't exist. Is this a problem?
+     */
 #if 0
-    /* TODO update name to mark it as a server timer? */
     add_test_watchdog(ev_hdl, pid, test->max_duration + TEST_SERVER_EXTRA_TIME,
             test->sigint, test->name);
 #endif
-    add_test_watchdog(ev_hdl, pid, TEST_SERVER_EXTRA_TIME, 0, "server");
+    //add_test_watchdog(ev_hdl, pid, TEST_SERVER_MAXIMUM_TIME, 0, "server");
 }
 
 
