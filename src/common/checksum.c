5a6729688d6810a143739822d591c301c3c1ac7e
Brendon Jones <brendonj@waikato.ac.nz>
2016-08-29 15:51:39 +1200

    Add GPLv2 licensing information to all source files.

--- src/common/checksum.c
+++ src/common/checksum.c
@@ -2,16 +2,7 @@
  * Copyright (c) 1988 IETF Trust, R. Braden, D.Borman, C. Partridge.
  * All rights reserved.
  *
- * The RFC is old enough to predate all the RFCs that discuss licensing. Code
- * samples in recent RFCs are available under the 3 clause BSD license. The
- * best I can find for RFC1071 is:
- *
- *     "Distribution of this memo is unlimited."
- *
- * and there is little point in putting code samples in such a document and
- * not expecting them to be used.
- *
- * TODO can we confirm what license this is under?
+ * From RFC 1071 "Computing the Internet Checksum" 1988.
  */
 
 
@@ -21,8 +12,7 @@
 
 
 /*
- * Calculate the icmp header checksum. Based on the example C code algorithm
- * given in RFC1071 "Computing the Internet Checksum".
+ * Calculate the icmp header checksum.
  */
 uint16_t checksum(uint16_t *data, int length) {
     uint64_t sum = 0;
