372a8fcf9aa8b6e8a206566f07eb2ee973b8a91b
Brendon Jones <brendonj@waikato.ac.nz>
2016-09-14 14:35:53 +1200

    Add further checks to scheduling unit test around premature tests.

--- src/measured/test/schedule_time_test.c
+++ src/measured/test/schedule_time_test.c
@@ -49,6 +49,7 @@ typedef struct test_schedule {
     uint64_t start;
     uint64_t end;
     uint64_t freq;
+    int run;
 } test_schedule_t;
 
 
@@ -173,59 +174,83 @@ static void check_next_schedule_time(void) {
     struct test_schedule schedule[] = {
         /* daily */
         /* period start */
-        {SCHEDULE_PERIOD_DAILY, {0, 0}, 0, 86400000, 30000},
+        {SCHEDULE_PERIOD_DAILY, {0, 0}, 0, 86400000, 30000, 0},
         /* period end */
-        {SCHEDULE_PERIOD_DAILY, {30, 0}, 0, 86400000, 30000},
+        {SCHEDULE_PERIOD_DAILY, {30, 0}, 0, 86400000, 30000, 0},
         /* 1s into period */
-        {SCHEDULE_PERIOD_DAILY, {1, 0}, 0, 86400000, 60000},
+        {SCHEDULE_PERIOD_DAILY, {1, 0}, 0, 86400000, 60000, 0},
         /* 1s before period end */
-        {SCHEDULE_PERIOD_DAILY, {89, 0}, 0, 86400000, 90000},
+        {SCHEDULE_PERIOD_DAILY, {89, 0}, 0, 86400000, 90000, 0},
         /* 0.5s into period */
-        {SCHEDULE_PERIOD_DAILY, {0, 500000}, 0, 86400000, 30000},
+        {SCHEDULE_PERIOD_DAILY, {0, 500000}, 0, 86400000, 30000, 0},
         /* 10.9s into period */
-        {SCHEDULE_PERIOD_DAILY, {10, 900000}, 0, 86400000, 60000},
+        {SCHEDULE_PERIOD_DAILY, {10, 900000}, 0, 86400000, 60000, 0},
         /* very early end time */
-        {SCHEDULE_PERIOD_DAILY, {9, 900000}, 0, 10000, 10000},
+        {SCHEDULE_PERIOD_DAILY, {9, 900000}, 0, 10000, 10000, 0},
         /* very early end time */
-        {SCHEDULE_PERIOD_DAILY, {10, 900000}, 0, 10000, 10000},
+        {SCHEDULE_PERIOD_DAILY, {10, 900000}, 0, 10000, 10000, 0},
         /* early end time, cycle */
-        {SCHEDULE_PERIOD_DAILY, {86398, 123000}, 0, 80000000, 10000},
+        {SCHEDULE_PERIOD_DAILY, {86398, 123000}, 0, 80000000, 10000, 0},
         /* cycle to next period */
-        {SCHEDULE_PERIOD_DAILY, {86398, 123000}, 0, 86400000, 10000},
+        {SCHEDULE_PERIOD_DAILY, {86398, 123000}, 0, 86400000, 10000, 0},
         /* frequency 0 */
-        {SCHEDULE_PERIOD_DAILY, {123, 123000}, 0, 0, 0},
+        {SCHEDULE_PERIOD_DAILY, {123, 123000}, 0, 0, 0, 0},
         /* freq 0, late start */
-        {SCHEDULE_PERIOD_DAILY, {35, 987000}, 60000, 0, 0},
+        {SCHEDULE_PERIOD_DAILY, {35, 987000}, 60000, 0, 0, 0},
         /* late start before time */
-        {SCHEDULE_PERIOD_DAILY, {61, 0}, 360000, 86400000, 60000},
+        {SCHEDULE_PERIOD_DAILY, {61, 0}, 360000, 86400000, 60000, 0},
         /* late start after time */
-        {SCHEDULE_PERIOD_DAILY, {361, 0}, 360000, 86400000, 60000},
+        {SCHEDULE_PERIOD_DAILY, {361, 0}, 360000, 86400000, 60000, 0},
 
         /* weekly */
         /* period start */
-        {SCHEDULE_PERIOD_WEEKLY, {0, 0}, 0, 604800000, 30000},
+        {SCHEDULE_PERIOD_WEEKLY, {0, 0}, 0, 604800000, 30000, 0},
         /* period end */
-        {SCHEDULE_PERIOD_WEEKLY, {30, 0}, 0, 604800000, 30000},
+        {SCHEDULE_PERIOD_WEEKLY, {30, 0}, 0, 604800000, 30000, 0},
         /* 1s into period */
-        {SCHEDULE_PERIOD_WEEKLY, {1, 0}, 0, 604800000, 60000},
+        {SCHEDULE_PERIOD_WEEKLY, {1, 0}, 0, 604800000, 60000, 0},
         /* 1s before period end */
-        {SCHEDULE_PERIOD_WEEKLY, {604799, 0}, 0, 604800000, 90000},
+        {SCHEDULE_PERIOD_WEEKLY, {604799, 0}, 0, 604800000, 90000, 0},
         /* 0.567890 into period */
-        {SCHEDULE_PERIOD_WEEKLY, {0, 567000}, 0, 604800000, 30000},
+        {SCHEDULE_PERIOD_WEEKLY, {0, 567000}, 0, 604800000, 30000, 0},
         /* 10.9s into period */
-        {SCHEDULE_PERIOD_WEEKLY, {10, 900000}, 0, 604800000, 60000},
+        {SCHEDULE_PERIOD_WEEKLY, {10, 900000}, 0, 604800000, 60000, 0},
         /* 1 day frequency */
-        {SCHEDULE_PERIOD_WEEKLY, {68, 500000}, 0, 604800000, 86400000},
+        {SCHEDULE_PERIOD_WEEKLY, {68, 500000}, 0, 604800000, 86400000, 0},
         /* last cycle in period */
-        {SCHEDULE_PERIOD_WEEKLY, {10000, 0}, 0, 10000000, 10000},
+        {SCHEDULE_PERIOD_WEEKLY, {10000, 0}, 0, 10000000, 10000, 0},
         /* frequency 0 */
-        {SCHEDULE_PERIOD_WEEKLY, {123, 123000}, 0, 0, 0},
+        {SCHEDULE_PERIOD_WEEKLY, {123, 123000}, 0, 0, 0, 0},
         /* freq 0, late start */
-        {SCHEDULE_PERIOD_WEEKLY, {86000, 987000}, 86400000, 0, 0},
+        {SCHEDULE_PERIOD_WEEKLY, {86000, 987000}, 86400000, 0, 0, 0},
         /* late start before time */
-        {SCHEDULE_PERIOD_WEEKLY, {61, 0}, 360000, 604800000, 60000},
+        {SCHEDULE_PERIOD_WEEKLY, {61, 0}, 360000, 604800000, 60000, 0},
         /* late start after time*/
-        {SCHEDULE_PERIOD_WEEKLY, {361, 0}, 360000, 86400000, 60000},
+        {SCHEDULE_PERIOD_WEEKLY, {361, 0}, 360000, 86400000, 60000, 0},
+
+        /* early triggered tests */
+        /* 1s early, not already run */
+        {SCHEDULE_PERIOD_DAILY, {-1, 0}, 0, 86400000, 30000, 0},
+        /* 1s early, already run */
+        {SCHEDULE_PERIOD_DAILY, {-1, 0}, 0, 86400000, 30000, 1},
+        /* just on the boundary of clock fudging, not already run  */
+        {SCHEDULE_PERIOD_DAILY, {0,-SCHEDULE_CLOCK_FUDGE+1}, 0, 9000, 3000, 0},
+        {SCHEDULE_PERIOD_DAILY, {0,-SCHEDULE_CLOCK_FUDGE}, 0, 9000, 3000, 0},
+        {SCHEDULE_PERIOD_DAILY, {0,-SCHEDULE_CLOCK_FUDGE-1}, 0, 9000, 3000, 0},
+        /* just on the boundary of clock fudging, already run  */
+        {SCHEDULE_PERIOD_DAILY, {0,-SCHEDULE_CLOCK_FUDGE+1}, 0, 9000, 3000, 1},
+        {SCHEDULE_PERIOD_DAILY, {0,-SCHEDULE_CLOCK_FUDGE}, 0, 9000, 3000, 1},
+        {SCHEDULE_PERIOD_DAILY, {0,-SCHEDULE_CLOCK_FUDGE-1}, 0, 9000, 3000, 1},
+        /* only slightly early, within fudge factor, not already run */
+        {SCHEDULE_PERIOD_DAILY, {0, -1}, 0, 86400000, 30000, 0},
+        /* only slightly early, within fudge factor, already run */
+        {SCHEDULE_PERIOD_DAILY, {0, -1}, 0, 86400000, 30000, 1},
+        /* only slightly early, within fudge factor, no freq, not already run */
+        {SCHEDULE_PERIOD_DAILY, {0, -1}, 0, 86400000, 0, 0},
+        {SCHEDULE_PERIOD_WEEKLY, {0, -1}, 0, 86400000, 0, 0},
+        /* only slightly early, within fudge factor, no freq, already run */
+        {SCHEDULE_PERIOD_DAILY, {0, -1}, 0, 86400000, 0, 1},
+        {SCHEDULE_PERIOD_WEEKLY, {0, -1}, 0, 86400000, 0, 1},
     };
     struct timeval expected[] = {
         /* daily */
@@ -256,6 +281,21 @@ static void check_next_schedule_time(void) {
         {399, 13000},
         {299, 0},
         {59, 0},
+        /* early */
+        {1, 0},
+        {1, 0},
+        {0, SCHEDULE_CLOCK_FUDGE-1},
+        {0, SCHEDULE_CLOCK_FUDGE},
+        {0, SCHEDULE_CLOCK_FUDGE+1},
+        {3, SCHEDULE_CLOCK_FUDGE-1},
+        {0, SCHEDULE_CLOCK_FUDGE},
+        {0, SCHEDULE_CLOCK_FUDGE+1},
+        {0, 1},
+        {30, 1},
+        {0, 1},
+        {0, 1},
+        {86400, 1},
+        {604800, 1},
     };
 
     memset(&ev_hdl, 0, sizeof(ev_hdl));
@@ -279,7 +319,7 @@ static void check_next_schedule_time(void) {
         /* get when the algorithm thinks the next scheduled test should be */
         offset = get_next_schedule_time(&ev_hdl, schedule[i].repeat,
                 schedule[i].start*1000, schedule[i].end*1000,
-                schedule[i].freq*1000, 0, NULL);
+                schedule[i].freq*1000, schedule[i].run, NULL);
 
         /*
         printf("%d\t%d.%d vs %d.%d\n", i, offset.tv_sec, offset.tv_usec,
