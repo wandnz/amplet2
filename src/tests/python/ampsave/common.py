f0d1a077111f27ea30434d1dec614064603ea6de
Brendon Jones <brendonj@waikato.ac.nz>
2016-09-28 14:25:36 +1300

    Remove extraneous semicolons from python code.

--- src/tests/python/ampsave/common.py
+++ src/tests/python/ampsave/common.py
@@ -57,45 +57,45 @@ def getPrintableDscp(value):
     if value == 0:
         return "Default"
     if value == 0b001000:
-        return "CS1";
+        return "CS1"
     if value == 0b010000:
-        return "CS2";
+        return "CS2"
     if value == 0b011000:
-        return "CS3";
+        return "CS3"
     if value == 0b100000:
-        return "CS4";
+        return "CS4"
     if value == 0b101000:
-        return "CS5";
+        return "CS5"
     if value == 0b110000:
-        return "CS6";
+        return "CS6"
     if value == 0b111000:
-        return "CS7";
+        return "CS7"
     if value == 0b001010:
-        return "AF11";
+        return "AF11"
     if value == 0b001100:
-        return "AF12";
+        return "AF12"
     if value == 0b001110:
-        return "AF13";
+        return "AF13"
     if value == 0b010010:
-        return "AF21";
+        return "AF21"
     if value == 0b010100:
-        return "AF22";
+        return "AF22"
     if value == 0b010110:
-        return "AF23";
+        return "AF23"
     if value == 0b011010:
-        return "AF31";
+        return "AF31"
     if value == 0b011100:
-        return "AF32";
+        return "AF32"
     if value == 0b011110:
-        return "AF33";
+        return "AF33"
     if value == 0b100010:
-        return "AF41";
+        return "AF41"
     if value == 0b100100:
-        return "AF42";
+        return "AF42"
     if value == 0b100110:
-        return "AF43";
+        return "AF43"
     if value == 0b101100:
-        return "VA";
+        return "VA"
     if value == 0b101110:
-        return "EF";
+        return "EF"
     return "0x%.02x" % value
