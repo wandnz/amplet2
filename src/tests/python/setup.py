069b84f84ccdd3cd628da231d0a4789f4326b03a
Brendon Jones <brendonj@waikato.ac.nz>
2018-07-30 17:55:54 +1200

    Bump version number to 0.8.3 and update changelog.

--- src/tests/python/setup.py
+++ src/tests/python/setup.py
@@ -4,7 +4,7 @@ except ImportError:
     from distutils.core import setup
 
 setup(name="ampsave",
-    version="0.8.2",
+    version="0.8.3",
     description="AMP server data storage package",
     author="Brendon Jones",
     author_email='contact@wand.net.nz',
