f5a65f420ab820c655473717b3d471fffbf77b86
Brendon Jones <brendonj@waikato.ac.nz>
2019-05-21 15:22:09 +1200

    Use GitLab CI/CD to build RPM packages and upload to bintray.

--- bintray-upload.sh
+++ bintray-upload.sh
@@ -2,10 +2,11 @@
 
 set -e -o pipefail
 
-BINTRAY_REPO="wand/amp"
+BINTRAY_DEB_REPO="wand/amp"
+BINTRAY_RPM_REPO="wand/amp-rpm"
 BINTRAY_LICENSE="GPL-2.0"
 
-apt-get update && apt-get install -y curl
+apt-get update && apt-get install -y curl rpm
 
 curl --silent -fL -XGET \
     "https://api.bintray.com/content/jfrog/jfrog-cli-go/\$latest/jfrog-cli-linux-amd64/jfrog?bt_package=jfrog-cli-linux-amd64" \
@@ -25,10 +26,23 @@ EOF
 
 for path in `find built-packages/ -maxdepth 1 -type d`; do
     IFS=_ read linux_version <<< $(basename "${path}")
-    for deb in `find "${path}" -maxdepth 1 -type f`; do
-        pkg_filename=$(basename "${deb}")
-        IFS=_ read pkg_name pkg_version pkg_arch <<< $(basename -s ".deb" "${pkg_filename}")
-        jfrog bt package-create --licenses "${BINTRAY_LICENSE}" --vcs-url "${CI_PROJECT_URL}" "${BINTRAY_REPO}/${pkg_name}" || true
-        jfrog bt upload --publish --deb "${linux_version}/main/${pkg_arch}" "${deb}" "${BINTRAY_REPO}/${pkg_name}/${pkg_version}" "pool/${linux_version}/main/${pkg_name}/"
+    for pkg in `find "${path}" -maxdepth 1 -type f`; do
+        pkg_extension=${pkg##*.}
+        pkg_filename=$(basename "${pkg}")
+
+        if [ "$pkg_extension" = "deb" ]; then
+            IFS=_ read pkg_name pkg_version pkg_arch <<< $(basename -s ".deb" "${pkg_filename}")
+            jfrog bt package-create --licenses "${BINTRAY_LICENSE}" --vcs-url "${CI_PROJECT_URL}" "${BINTRAY_DEB_REPO}/${pkg_name}" || true
+            jfrog bt upload --publish --deb "${linux_version}/main/${pkg_arch}" "${pkg}" "${BINTRAY_DEB_REPO}/${pkg_name}/${pkg_version}" "pool/${linux_version}/main/${pkg_name}/"
+        fi
+
+        if [ "$pkg_extension" = "rpm" ]; then
+            IFS=_ read rpm_distribution rpm_releasever <<< $linux_version
+            read pkg_name pkg_version pkg_arch <<< $(rpm -pq ${pkg} --queryformat "%{NAME} %{VERSION}-%{RELEASE} %{ARCH}")
+            pkg_version=${pkg_version%.*}
+
+            jfrog bt package-create --licenses "${BINTRAY_LICENSE}" --vcs-url "${CI_PROJECT_URL}" "${BINTRAY_RPM_REPO}/${pkg_name}" || true
+            jfrog bt upload --publish "${pkg}" "${BINTRAY_RPM_REPO}/${pkg_name}/${pkg_version}" "${rpm_distribution}/${rpm_releasever}/${pkg_arch}/Packages/${pkg_name}/"
+        fi
     done
 done
---
+++ gitlab-build-rpm.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -x -e -o pipefail
+
+. /etc/os-release
+CODENAME=${ID}_${VERSION_ID}
+TAGNAME=`echo ${CI_COMMIT_REF_NAME} | cut -d '-' -f 1`
+
+# add the repository that has our dependencies
+cat << EOF > /etc/yum.repos.d/bintray-wand-amp-rpm.repo
+#bintray-wand-amp-rpm - packages by wand from Bintray
+[bintray-wand-amp-rpm]
+name=bintray-wand-amp-rpm
+baseurl=https://dl.bintray.com/wand/amp-rpm/${ID}/\$releasever/\$basearch/
+gpgkey=https://bintray.com/user/downloadSubjectPublicKey?username=wand
+gpgcheck=0
+repo_gpgcheck=1
+enabled=1
+EOF
+
+# set up an RPM build environment
+yum install -y rpm-build rpmdevtools make gcc epel-release
+rpmdev-setuptree
+yum-builddep -y rpm/${CI_PROJECT_NAME}.spec
+
+# create a tarball to build the RPM from
+./bootstrap.sh
+./configure
+make dist
+
+# copy it into position
+cp ${CI_PROJECT_NAME}-*.tar.gz ~/rpmbuild/SOURCES/${TAGNAME}.tar.gz
+cp rpm/*.patch ~/rpmbuild/SOURCES/ || true
+cp rpm/${CI_PROJECT_NAME}.spec ~/rpmbuild/SPECS/
+
+# build the RPM
+cd ~/rpmbuild/
+rpmbuild -bb --define "debug_package %{nil}" SPECS/${CI_PROJECT_NAME}.spec
+
+# move the built RPM into position
+mkdir -p ${CI_PROJECT_DIR}/built-packages/${CODENAME}/ || true
+mv ~/rpmbuild/RPMS/*/*.rpm ${CI_PROJECT_DIR}/built-packages/${CODENAME}/
