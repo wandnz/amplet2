1035bab22765d6232f22cf7506021b8642398727
Brendon Jones <brendonj@waikato.ac.nz>
2019-11-12 11:34:20 +1300

    Build all targets and test all amd64 targets when CI/CD run via web.

--- gitlab-test-deb.sh
+++ gitlab-test-deb.sh
@@ -5,6 +5,25 @@ set -x -e -o pipefail
 # TODO check an upgrade from the previous version as well as a fresh install
 # TODO is the null virt-server the most appropriate one to use here?
 
+CODENAME=`lsb_release -c -s`
+ARCH=`dpkg --print-architecture`
+
+case "$CODENAME" in
+    jessie)
+        echo "deb http://archive.debian.org/debian jessie-backports main" > /etc/apt/sources.list.d/jessie-backports.list
+        echo "Acquire::Check-Valid-Until \"false\";" >> /etc/apt/apt.conf
+        apt-get update
+        apt-get -t jessie-backports install -y autopkgtest
+        ;;
+
+    xenial)
+        apt-get -t xenial-backports install -y autopkgtest
+        ;;
+
+    *)
+        apt-get install -y autopkgtest
+        ;;
+esac
+
 # autopkgtest will run tests based on entries in debian/tests/control
-apt-get install -y autopkgtest
-autopkgtest built-packages/$CODENAME/amplet2-client_*deb -- null
+autopkgtest built-packages/$CODENAME/amplet2-client_*${ARCH}.deb -- null
