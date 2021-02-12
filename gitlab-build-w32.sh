d7244f2f0ded7f76e527edb1d542b44ad63e876c
Brendon Jones <brendonj@waikato.ac.nz>
2021-02-12 16:54:03 +1300

    Fetch Windows dependencies from a location under our control.

--- gitlab-build-w32.sh
+++ gitlab-build-w32.sh
@@ -7,7 +7,9 @@ apt-get -y upgrade
 apt-get install -y apt-transport-https automake autotools-dev ca-certificates flex libtool make mingw-w64 protobuf-compiler protobuf-c-compiler python wget wixl xz-utils zstd
 
 VERSION=`grep -m 1 ProductVersion w32/amplet2-client.wxs | awk -F \" '{print $2}'`
-REPO="https://repo.msys2.org/mingw/x86_64/"
+# files originally from https://repo.msys2.org/mingw/x86_64/ but they only
+# keep the newest version which breaks our builds, so we mirror them
+REPO="https://wand.net.nz/~brendonj/amp/mingw/"
 DEPS="
     mingw-w64-x86_64-protobuf-c-1.3.3-1-any.pkg.tar.xz
     mingw-w64-x86_64-rabbitmq-c-0.10.0-1-any.pkg.tar.xz
@@ -15,16 +17,16 @@ DEPS="
     mingw-w64-x86_64-curl-7.71.0-1-any.pkg.tar.zst
     mingw-w64-x86_64-unbound-1.10.0-1-any.pkg.tar.xz
     mingw-w64-x86_64-openssl-1.1.1.g-1-any.pkg.tar.xz
-    mingw-w64-x86_64-confuse-3.2.2-1-any.pkg.tar.xz
-    mingw-w64-x86_64-libevent-2.1.11-2-any.pkg.tar.xz
+    mingw-w64-x86_64-confuse-3.3-1-any.pkg.tar.zst
+    mingw-w64-x86_64-libevent-2.1.12-1-any.pkg.tar.zst
     mingw-w64-x86_64-gettext-0.19.8.1-9-any.pkg.tar.zst
-    mingw-w64-x86_64-libiconv-1.16-1-any.pkg.tar.xz
-    mingw-w64-x86_64-zlib-1.2.11-7-any.pkg.tar.xz
-    mingw-w64-x86_64-brotli-1.0.7-4-any.pkg.tar.xz
+    mingw-w64-x86_64-libiconv-1.16-2-any.pkg.tar.zst
+    mingw-w64-x86_64-zlib-1.2.11-9-any.pkg.tar.zst
+    mingw-w64-x86_64-brotli-1.0.9-2-any.pkg.tar.zst
     mingw-w64-x86_64-libidn2-2.3.0-1-any.pkg.tar.xz
     mingw-w64-x86_64-libunistring-0.9.10-2-any.pkg.tar.zst
     mingw-w64-x86_64-nghttp2-1.41.0-1-any.pkg.tar.zst
-    mingw-w64-x86_64-libpsl-0.21.0-2-any.pkg.tar.xz
+    mingw-w64-x86_64-libpsl-0.21.1-2-any.pkg.tar.zst
     mingw-w64-x86_64-libssh2-1.9.0-2-any.pkg.tar.zst
     "
 
