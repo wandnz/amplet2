4606c7c72657832f8d76b2e056274b15be529915
Brendon Jones <brendonj@waikato.ac.nz>
2021-12-17 15:13:03 +1300

    Update capabilities test to match new getcap output format.

--- tests/test_binaries.sh
+++ tests/test_binaries.sh
@@ -2,19 +2,28 @@
 
 . tests/test_helper.sh
 
+operator="+"
+allcaps=" = "
+
+# getcap output changed in v2.41, which is currently only used by bullseye
+if [ "$CODENAME" = "bullseye" ]; then
+    operator="="
+    allcaps=" "
+fi
+
 # check capabilities of installed binaries are correct
 test_capabilities() {
     declare -A capabilities=(
-        ["/usr/sbin/amplet2"]="cap_net_bind_service,cap_net_admin,cap_net_raw+ep"
+        ["/usr/sbin/amplet2"]="cap_net_bind_service,cap_net_admin,cap_net_raw${operator}ep"
         ["/usr/bin/amp-dns"]=""
         ["/usr/bin/amp-external"]=""
-        ["/usr/bin/amp-fastping"]="cap_net_raw+ep"
+        ["/usr/bin/amp-fastping"]="cap_net_raw${operator}ep"
         ["/usr/bin/amp-http"]=""
-        ["/usr/bin/amp-icmp"]="cap_net_raw+ep"
-        ["/usr/bin/amp-tcpping"]="cap_net_admin,cap_net_raw+ep"
-        ["/usr/bin/amp-throughput"]="cap_net_bind_service+ep"
-        ["/usr/bin/amp-trace"]="cap_net_raw+ep"
-        ["/usr/bin/amp-udpstream"]="cap_net_bind_service+ep"
+        ["/usr/bin/amp-icmp"]="cap_net_raw${operator}ep"
+        ["/usr/bin/amp-tcpping"]="cap_net_admin,cap_net_raw${operator}ep"
+        ["/usr/bin/amp-throughput"]="cap_net_bind_service${operator}ep"
+        ["/usr/bin/amp-trace"]="cap_net_raw${operator}ep"
+        ["/usr/bin/amp-udpstream"]="cap_net_bind_service${operator}ep"
         ["/usr/bin/amplet2-remote"]=""
         )
 
@@ -26,7 +35,7 @@ test_capabilities() {
         if [ -z "$cap" ]; then
             assertEquals "" "$out"
         else
-            assertEquals "${bin} = ${cap}" "$out"
+            assertEquals "${bin}${allcaps}${cap}" "$out"
         fi
     done
 }
